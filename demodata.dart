0e5f7df4b0e2d66c97ae132e403ed31df7f46379
baselallam <baseljahen@gmail.com>
2021-08-05 14:16:55 +0200

    'commit'

---
+++ demodata.dart
@@ -0,0 +1,55 @@
+
+List<Map<String, dynamic>> player = [
+  {
+        'name' : 'ahmed',
+        'email' : 'ahmed.com',
+        'scores' : {
+          'sub1' : 200,
+          'sub2' : 300
+        },
+        'fiends' : [
+          1, 2, 3
+        ],
+        'isSuccess' : true,
+        'familyMember' : [
+          {
+            'name' : 'ahmed father',
+            'age' : 50
+          },
+          {
+            'name' : 'ahmed mother',
+            'age' : 40
+          },
+          {
+            'name' : 'ahmed sister',
+            'age' : 20
+          },
+        ]
+      },
+    {
+        'name' : 'ali',
+        'email' : 'ali.com',
+        'scores' : {
+          'sub1' : 250,
+          'sub2' : 350
+        },
+        'fiends' : [
+          1, 2, 3
+        ],
+        'isSuccess' : false,
+        'familyMember' : [
+          {
+            'name' : 'ali father',
+            'age' : 50
+          },
+          {
+            'name' : 'ali mother',
+            'age' : 40
+          },
+          {
+            'name' : 'ali sister',
+            'age' : 20
+          },
+        ]
+      },
+];
--- kora.dart
+++ kora.dart
@@ -1,71 +1,28 @@
+import 'demodata.dart';
 
-playerData(Map<String, dynamic>? player) {
+playerData(Map<String, dynamic>? playerData) {
 
-  print('''
-    ${player!['name']}, 
-    Father : ${player['familyMember'][0]['name']},
-    mother : ${player['familyMember'][1]['name']},
-''');
+  print(
+    '''
+    Name Is: ${playerData!['name']}, 
+    '''
+  );
+}
+
+
+loopOnPlayerData(List<Map<String, dynamic>>? playerList) {
+
+  for(Map<String, dynamic> i in playerList!) {
+    playerData(i);
+    i['familyMember'].forEach((x) {
+      playerData(x);
+    });
+  }
 }
 
 
 void main() {
 
-  Map<int, Map<String, dynamic>> player = {
-    0 : {
-        'name' : 'ahmed',
-        'email' : 'ahmed.com',
-        'scores' : {
-          'sub1' : 200,
-          'sub2' : 300
-        },
-        'fiends' : [
-          1, 2, 3
-        ],
-        'isSuccess' : true,
-        'familyMember' : [
-          {
-            'name' : 'ali father',
-            'age' : 50
-          },
-          {
-            'name' : 'ali mother',
-            'age' : 40
-          },
-          {
-            'name' : 'ali sister',
-            'age' : 20
-          },
-        ]
-      },
-      1 : {
-        'name' : 'ali',
-        'email' : 'ali.com',
-        'scores' : {
-          'sub1' : 250,
-          'sub2' : 350
-        },
-        'fiends' : [
-          1, 2, 3
-        ],
-        'isSuccess' : false,
-        'familyMember' : [
-          {
-            'name' : 'ali father',
-            'age' : 50
-          },
-          {
-            'name' : 'ali mother',
-            'age' : 40
-          },
-          {
-            'name' : 'ali sister',
-            'age' : 20
-          },
-        ]
-      },
-  };
-
-  playerData(player[0]);
+  loopOnPlayerData(player);
   
 }
