0aaa17cca2c190eaf9b894c539163a686ab6b7bd
baselallam <baseljahen@gmail.com>
2021-08-07 13:36:49 +0200

    'commit'

--- kora.dart
+++ kora.dart
@@ -26,3 +26,36 @@ void main() {
   loopOnPlayerData(player);
   
 }
+
+
+
+
+// avg
+
+
+Map<int, List<num>> lists = {
+  1 : [
+    10, 20, 30
+  ],
+  2 : [
+    100, 200, 300
+  ],
+  3 : [
+    150, 250, 530
+  ],
+  4 : [
+    20, 60, 70
+  ]
+};
+
+
+getAvg(List x) {
+
+  num sum = x.reduce((value, element) => value + element);
+
+  num count = x.length;
+
+  num avg = sum / count;
+
+  print(avg);
+}
