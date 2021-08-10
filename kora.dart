83408ad40cb002eb64d323d6379da68d35bf6b8d
baselallam <baseljahen@gmail.com>
2021-08-10 10:51:42 +0200

    'commit'

--- kora.dart
+++ kora.dart
@@ -1,80 +1,81 @@
-import 'demodata.dart';
 
-playerData(Map<String, dynamic>? playerData) {
-
-  print(
-    '''
-    Name Is: ${playerData!['name']}, 
-    '''
-  );
-}
-
-
-loopOnPlayerData(List<Map<String, dynamic>>? playerList) {
-
-  for(Map<String, dynamic> i in playerList!) {
-    playerData(i);
-    i['familyMember'].forEach((x) {
-      playerData(x);
-    });
-  }
-}
-
-
-void main() {
-
-  // loopOnPlayerData(player);
-
-  // lists.forEach((key, value) {
-  //   getAvg(value);
-  // });
-
-  player.forEach((i) {
-    num sum = 0;
-    i['scores'].forEach((p, x) {
-      sum += x;
-    });
-    print(sum);
-  });
-  
-}
-
-
-
-
-// avg
-
-
-// Map<int, List<num>> lists = {
-//   1 : [
-//     10, 20, 30
-//   ],
-//   2 : [
-//     100, 200, 300
-//   ],
-//   3 : [
-//     150, 250, 530
-//   ],
-//   4 : [
-//     20, 60, 70
-//   ]
-// };
-
-
-// getAvg(List<num> x) {
-
-//   num sum = x.reduce((value, element) => value + element);
-
-//   num count = x.length;
-
-//   num avg = sum / count;
-
-//   print(avg);
-// }
-
-
-// 1- loop on list
-// 2- pointer on scores
-// 3- loop on scores values
-// 4- sum of scores values
-// 5- print
+List<Map<String, dynamic>> player = [
+  {
+        'name' : 'ahmed',
+        'email' : 'ahmed.com',
+        'scores' : {
+          'sub1' : 200,
+          'sub2' : 300
+        },
+        'fiends' : [
+          1, 2, 3
+        ],
+        'isSuccess' : false,
+        'familyMember' : [
+          {
+            'name' : 'ahmed father',
+            'age' : 50
+          },
+          {
+            'name' : 'ahmed mother',
+            'age' : 40
+          },
+          {
+            'name' : 'ahmed sister',
+            'age' : 20
+          },
+        ]
+      },
+    {
+        'name' : 'ali',
+        'email' : 'ali.com',
+        'scores' : {
+          'sub1' : 250,
+          'sub2' : 50
+        },
+        'fiends' : [
+          1, 2, 3
+        ],
+        'isSuccess' : true,
+        'familyMember' : [
+          {
+            'name' : 'ali father',
+            'age' : 50
+          },
+          {
+            'name' : 'ali mother',
+            'age' : 40
+          },
+          {
+            'name' : 'ali sister',
+            'age' : 20
+          },
+        ]
+      },
+      {
+        'name' : 'mohsen',
+        'email' : 'mohsen.com',
+        'scores' : {
+          'sub1' : 50,
+          'sub2' : 50
+        },
+        'fiends' : [
+          1, 2, 3
+        ],
+        'isSuccess' : true,
+        'familyMember' : [
+          {
+            'name' : 'mohsen father',
+            'age' : 50
+          },
+          {
+            'name' : 'mohsen mother',
+            'age' : 40
+          },
+          {
+            'name' : 'mohsen sister',
+            'age' : 20
+          },
+        ]
+      },
+];
