79a13f69d265eb2a2d0a8ea4b65db723be2872e1
baselallam <baseljahen@gmail.com>
2021-08-31 13:43:45 +0200

    'commit'

--- doctor/lib/homepage.dart
+++ doctor/lib/homepage.dart
@@ -9,10 +9,84 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  List<Map<String, dynamic>> drawerData = [
+    {
+      'txt' : 'Wishlist',
+      'icon' : Icons.favorite
+    },
+    {
+      'txt' : 'Appointment',
+      'icon' : Icons.bookmark
+    },
+    {
+      'txt' : 'Settings',
+      'icon' : Icons.settings
+    },
+    {
+      'txt' : 'About Us',
+      'icon' : Icons.info
+    },
+    {
+      'txt' : 'Contact Us',
+      'icon' : Icons.phone
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xff00c8d7),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text(
+          'Welcome',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        iconTheme: IconThemeData(color: Colors.black, size: 30.0),
+        actions: [
+          Icon(Icons.search)
+        ],
+      ),
+      drawer: Drawer(
+        child: Column(
+            children: [
+              DrawerHeader(
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15.0),
+                    image: DecorationImage(
+                      image: NetworkImage('https://img.freepik.com/free-photo/hand-painted-watercolor-background-with-sky-clouds-shape_24972-1095.jpg?size=626&ext=jpg'),
+                      fit: BoxFit.fill
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      minRadius: 30,
+                      maxRadius: 30,
+                      backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/44323531?v=4'),
+                      backgroundColor: Colors.white,
+                    ),
+                    title: Text('Bassel Allam', style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold)),
+                    subtitle: Text('My Profile', style: TextStyle(color: Colors.grey, fontSize: 18.0)),
+                    trailing: Icon(Icons.arrow_forward_ios, color: Colors.black, size: 20,),
+                  ),
+                ),
+              ),
+              for(Map<String, dynamic> i in drawerData)
+              ListTile(
+                leading: Icon(i['icon'], color: Colors.black, size: 30),
+                title: Text(i['txt'], style: TextStyle(color: Colors.black, fontSize: 20.0)),
+                trailing: Icon(Icons.arrow_forward_ios, color: Colors.black, size: 20,),
+              ),
+            ],
+        ),
+      )
     );
   }
 }
