75da4cc03d1a85e3c61f7ed7ef9f183c6d5dd309
baselallam <baseljahen@gmail.com>
2021-09-16 13:16:41 +0200

    'commit'

--- doctor/lib/main.dart
+++ doctor/lib/main.dart
@@ -19,13 +19,4 @@ class MyApp extends StatelessWidget {
 }
 
 
-// https://assets.materialup.com/uploads/31f248a8-5755-44bd-ba14-133fd3a5d83c/preview.png
-// https://img.freepik.com/free-psd/restaurant-food-delivery-mobile-app-ui-design_279025-18.jpg?size=626&ext=jpg
-
-// create project
-// make project arch ( folder , file )
-// write shared theme
-// main.dart 
-// start your first screen 
-// find the reuseable widget
-// start your second screen
+// https://cdn.sketchrepo.com/images/2x/freebie-design-concept-illustrator-3d-k8.jpg
